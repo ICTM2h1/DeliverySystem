c08f4d0e97a8333bf290f40037babf68140fcc49
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-04-16 10:09:41 +0200

    Fix coding standards

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-
+/**
+ * This class provides the main entry point for the application.
+ */
 public class Main {
 
+    /**
+     * Main entry point of this class.
+     *
+     * @param args
+     *   The given arguments.
+     */
     public static void main(String[] args) {
         System.out.println("Application entry point.");
     }
