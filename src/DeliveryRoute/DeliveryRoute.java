c02d0bfc4b194f2bf87b10398bde5f69cd8d0c72
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-04-26 16:14:29 +0200

    Keep track of the unsorted orders

--- src/DeliveryRoute/DeliveryRoute.java
+++ src/DeliveryRoute/DeliveryRoute.java
@@ -11,6 +11,10 @@ public class DeliveryRoute {
 
     private final ArrayList<HashMap<String, String>> orders;
 
+    public static int routeCount = 0;
+    public static int ordersCount = 0;
+    private static double ordersUnsorted = 0;
+
     /**
      * Creates a new delivery route object.
      *
@@ -20,6 +24,13 @@ public class DeliveryRoute {
         Orders orders = new Orders(date);
 
         this.orders = orders.filterOnGeometry();
+        routeCount++;
+        ordersCount += this.orders.size();
+        ordersUnsorted += (((double) orders.getWithoutGeometry()) / ((double) this.orders.size())) * 100;
+    }
+
+    public static double getOrdersUnsortedPercentage() {
+        return ordersUnsorted / (routeCount * 100) * 100;
     }
 
 }
