85d28ad1e37e8da88d8fb68bb15f003ba058a039
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-05-03 17:28:19 +0200

    Fix coding standards

--- src/main/java/UI/Components/Table.java
+++ src/main/java/UI/Components/Table.java
@@ -3,30 +3,52 @@ package UI.Components;
 import javax.swing.*;
 import java.util.ArrayList;
 
+/**
+ * Provides a table component.
+ */
 public class Table {
 
     private ArrayList<ArrayList<String>> data;
     private ArrayList<String> columns;
 
+    /**
+     * Creates a new table.
+     */
     public Table() {
         this.data = new ArrayList<>();
         this.columns = new ArrayList<>();
     }
 
+    /**
+     * Creates a new table.
+     *
+     * @param columns The columns.
+     */
     public Table(ArrayList<String> columns) {
         this.columns = columns;
     }
 
+    /**
+     * Creates a new table.
+     *
+     * @param data The data.
+     * @param columns The columns.
+     */
     public Table(ArrayList<ArrayList<String>> data, ArrayList<String> columns) {
         this.data = data;
         this.columns = columns;
     }
 
+    /**
+     * Renders the table to a component.
+     *
+     * @return Table component.
+     */
     public JScrollPane render() {
         String[][] tableData = new String[this.data.size()][];
-        for (int i = 0; i < this.data.size(); i++) {
-            ArrayList<String> row = this.data.get(i);
-            tableData[i] = row.toArray(new String[0]);
+        for (int delta = 0; delta < this.data.size(); delta++) {
+            ArrayList<String> row = this.data.get(delta);
+            tableData[delta] = row.toArray(new String[0]);
         }
 
         JTable table = new JTable(tableData, this.columns.toArray());
@@ -39,10 +61,20 @@ public class Table {
         return scrollPane;
     }
 
+    /**
+     * Adds a column to the table header.
+     *
+     * @param column The name of the column.
+     */
     public void addColumn(String column) {
         this.columns.add(column);
     }
 
+    /**
+     * Adds a row to the data of the table.
+     *
+     * @param data The data of the row.
+     */
     public void addRow(ArrayList<String> data) {
         this.data.add(data);
     }
