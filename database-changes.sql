64b370f8c47a0049fd99e6d4bcb75a1e6593022b
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-05-03 10:01:27 +0200

    Add test user

--- database-changes.sql
+++ database-changes.sql
@@ -36,3 +36,19 @@ INSERT INTO `nerdygadgets`.`people` (
     0, '+310612345678', 'admin@admin.nl',
     '2020-12-8 23:59:59', '9999-12-31 23:59:59', 1, 2
 );
+
+/** Password for this user is 'nimda' */
+INSERT INTO `nerdygadgets`.`people` (
+    FullName, PreferredName, SearchName,
+    IsPermittedToLogon, LogonName,
+    IsExternalLogonProvider, HashedPassword,
+    IsSystemUser, IsEmployee, IsSalesperson,
+    PhoneNumber, EmailAddress, ValidFrom,
+    ValidTo, LastEditedBy
+) VALUES (
+    'test', 'test', 'test',
+    1, 'test', 0,
+    '$2y$10$.D3CZ9FSjEYCOoZwlr.WjekQaijWBo4KTW0I3rpgm4Ou60cknIIXi', 0, 1,
+    0, '+310612345678', 'test@test.nl',
+    '2020-12-8 23:59:59', '9999-12-31 23:59:59', 1
+);
